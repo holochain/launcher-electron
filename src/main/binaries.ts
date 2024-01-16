f0f280eaa34c0c3d87b0adb7211a9fa43402fa7f
Matthias <tuttiacc@hotmail.com>
2024-01-16 13:00:26 +0100

    bumped to 0.3.0-beta-dev.32

--- src/main/binaries.ts
+++ src/main/binaries.ts
@@ -1,14 +1,14 @@
 import { app } from 'electron';
 import * as path from 'path';
 
-export const DEFAULT_HOLOCHAIN_VERSION = '0.2.3';
+export const DEFAULT_HOLOCHAIN_VERSION = '0.3.0-beta-dev.32';
 
 const BINARIES_DIRECTORY = app.isPackaged
   ? path.join(app.getAppPath(), '../app.asar.unpacked/resources/bins')
   : path.join(app.getAppPath(), './resources/bins');
 
 const HOLOCHAIN_BINARIES = {
-  '0.2.3': path.join(
+  '0.3.0-beta-dev.32': path.join(
     BINARIES_DIRECTORY,
     `holochain-v${DEFAULT_HOLOCHAIN_VERSION}${process.platform === 'win32' ? '.exe' : ''}`,
   ),
@@ -16,7 +16,7 @@ const HOLOCHAIN_BINARIES = {
 
 const LAIR_BINARY = path.join(
   BINARIES_DIRECTORY,
-  `lair-keystore-v0.3.0${process.platform === 'win32' ? '.exe' : ''}`,
+  `lair-keystore-v0.4.0${process.platform === 'win32' ? '.exe' : ''}`,
 );
 
 export { HOLOCHAIN_BINARIES, LAIR_BINARY };
