ff8ee07078b39c1a04799526fbd54d68155f8f45
Matthias <tuttiacc@hotmail.com>
2024-01-26 17:35:32 +0100

    fixed js-client version

--- src/main/binaries.ts
+++ src/main/binaries.ts
@@ -1,14 +1,14 @@
 import { app } from 'electron';
 import * as path from 'path';
 
-export const DEFAULT_HOLOCHAIN_VERSION = '0.3.0-beta-dev-34-9da122';
+export const DEFAULT_HOLOCHAIN_VERSION = '0.3.0-beta-dev.34-9da122';
 
 const BINARIES_DIRECTORY = app.isPackaged
   ? path.join(app.getAppPath(), '../app.asar.unpacked/resources/bins')
   : path.join(app.getAppPath(), './resources/bins');
 
 const HOLOCHAIN_BINARIES = {
-  '0.3.0-beta-dev-34-9da122': path.join(
+  '0.3.0-beta-dev.34-9da122': path.join(
     BINARIES_DIRECTORY,
     `holochain-v${DEFAULT_HOLOCHAIN_VERSION}${process.platform === 'win32' ? '.exe' : ''}`,
   ),
